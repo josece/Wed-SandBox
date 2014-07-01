206447851b21a5b3cb503cbc02dd2ffc6a70d652
josece <josece@gmail.com>
2014-06-30 19:13:50 -0500

    Documentacion

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -11,22 +11,25 @@ class UsersController extends \BaseController {
 	 
 
 	protected $layout = "layouts.main";
-  	public function __construct() {
-        $this->beforeFilter('auth', array('except' =>  array('getLogin' ,'getRegister','postSignin' )));
-    }
+	
 	/**
-	 * Display a listing of the resource.
+	 * Builds object user
 	 *
-	 * @return Response
 	 */
-	public function index()
-	{
-		//$this->layout->user = Auth::user();
-		//
-		//$users = User::all();
+
+  	public function __construct() {
+        $this->beforeFilter('auth', 
+        	array('except' =>  
+        		array('getLogin' ,
+        			'getRegister',
+        			'postSignin' 
+        		)
+        	)
+        );
+    }
+	
+	public function index(){
 		
-		//$users = User::paginate(5);
-		//$this->layout->content = View::make('users.index', compact('users'));
 	}
 	 public function getRegister() {
 	 	//si tienen ya una sesion iniciada, no se puede ver el formulario de login
@@ -39,7 +42,12 @@ class UsersController extends \BaseController {
 	 
 	 
 	
-
+	/**
+	 * Gets Facebook object
+	 *
+	 * @param  
+	 * @return redirect
+	 */
 	public function getFacebookauth()
 	{
 		
@@ -101,68 +109,79 @@ class UsersController extends \BaseController {
            ->with('alert', 'There were validation errors.');
 	}
 
-
-
+	/**
+	 * Gets the data from the Create User Form, creates user and stores to database 
+	 * @return redirect
+	 */
 	public function postCreate() {
-			$validator = Validator::make(Input::all(), User::$rules);
+		$validator = Validator::make(Input::all(), User::$rules);
 
-			if ($validator->passes()) {
-				$user = new User;
-				$user->firstname = Input::get('firstname');
-				$user->lastname = Input::get('lastname');
-				$user->email = Input::get('email');
-				$user->password = Hash::make(Input::get('password'));
-				$user->save();
+		if ($validator->passes()) {
+			$user = new User;
+			$user->firstname = Input::get('firstname');
+			$user->lastname = Input::get('lastname');
+			$user->email = Input::get('email');
+			$user->password = Hash::make(Input::get('password'));
+			$user->save();
 
-				return Redirect::to('user/login')->with('message', 'Thanks for registering!');
-			} else {
-				return Redirect::to('user/register')->with('message', 'The following errors occurred')->withErrors($validator)->withInput();
-			}
+			return Redirect::to('user/login')->with('message', 'Thanks for registering!');
+		} else {
+			return Redirect::to('user/register')->with('message', 'The following errors occurred')->withErrors($validator)->withInput();
 		}
-
-		public function getLogin() {
-			//si tienen ya una sesion iniciada, no se puede ver el formulario de login
-			if (Auth::check()) {
-				return Redirect::to('user/dashboard');
-			}
-			$this->layout->content = View::make('users.login');
+	}
+	
+	/**
+	 * Sets the layout content with the login view. 
+	 */
+	public function getLogin() {
+		//si tienen ya una sesion iniciada, no se puede ver el formulario de login
+		if (Auth::check()) {
+			return Redirect::to('user/dashboard');
 		}
+		$this->layout->content = View::make('users.login');
+	}
 
-		public function postSignin() {
-			if (Auth::attempt(array('email'=>Input::get('email'), 'password'=>Input::get('password')))) {
-				
-				
-				return Redirect::to('user/dashboard');//->with('message', 'You are now logged in!');
-			} else {
-				return Redirect::to('user/login')
-					->with('alert', 'Your username/password combination was incorrect')
-					->withInput();
-			}
+	/**
+	 * Gets the sign in data and attempts to login. 
+	 * @return Redirect
+	 */
+	public function postSignin() {
+		if (Auth::attempt(array('email'=>Input::get('email'), 'password'=>Input::get('password')))) {
+			return Redirect::to('user/dashboard');//->with('message', 'You are now logged in!');
+		} else {
+			return Redirect::to('user/login')
+				->with('alert', 'Your username/password combination was incorrect')
+				->withInput();
 		}
-
-		public function getDashboard() {
-				$this->layout->content = View::make('users.dashboard');
-		}
-
-		public function getLogout() {
-			Auth::logout();
-			return Redirect::to('user/login');//->with('message', 'Your are now logged out!');
-		}
+	}
+	/**
+	 * Sets the layout content with the login view. 
+	 */
+	public function getDashboard() {
+		$this->layout->content = View::make('users.dashboard');
+	}
+	/**
+	 * Logouts user
+	 */
+	public function getLogout() {
+		Auth::logout();
+		return Redirect::to('user/login');//->with('message', 'Your are now logged out!');
+	}
 		
-		
-		/*
-		* Facebook Auth Callback
-		*/
-		public function getFacebookcallback() {
-		    $code = Input::get('code');
-		    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
- 
-		    $facebook = new Facebook(Config::get('facebook'));
-		    $uid = $facebook->getUser();
- 
-		    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
- 
-		    $me = $facebook->api('/me');
+
+	/*
+	 * Facebook Auth Callback
+	 */
+	public function getFacebookcallback() {
+		$code = Input::get('code');
+		if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+
+		$facebook = new Facebook(Config::get('facebook'));
+		$uid = $facebook->getUser();
+
+		if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+
+		$me = $facebook->api('/me');
 			/*
 			Los valores que recibimos de Facebook son un arreglo $me: 
 			array(11) { ["id"],
@@ -179,32 +198,33 @@ class UsersController extends \BaseController {
 			}
 		 */
 			//dd($me); //equivalente a un var_dump
-		    $profile = Profile::whereUid($uid)->first();
-		    if (empty($profile)) {
- 
-		        $user = new User;
+			$profile = Profile::whereUid($uid)->first();
+			if (empty($profile)) {
+
+				$user = new User;
 				$user->firstname = $me['first_name'];
 				$user->lastname = $me['last_name'];
 		        //$user->name = $me['first_name'].' '.$me['last_name'];
-		        $user->email = $me['email'];
-		        $user->photo = 'https://graph.facebook.com/'.$uid.'/picture?type=large';
-		        $user->save();
- 
-		        $profile = new Profile();
-		        $profile->uid = $uid;
-		        $profile->username = $uid;
-		        $profile = $user->profiles()->save($profile);
-		    }
- 
-		    $profile->access_token = $facebook->getAccessToken();
-		    $profile->save();
- 
-		    $user = $profile->user;
- 
-		    Auth::login($user);
- 			return Redirect::to('user/dashboard');
+				$user->email = $me['email'];
+				$user->photo = 'https://graph.facebook.com/'.$uid.'/picture?type=large';
+				$user->save();
+
+				$profile = new Profile();
+				$profile->uid = $uid;
+				$profile->username = $uid;
+				$profile = $user->profiles()->save($profile);
+			}
+
+			$profile->access_token = $facebook->getAccessToken();
+			$profile->save();
+
+			$user = $profile->user;
+
+			Auth::login($user);
+			return Redirect::to('user/dashboard');
 		    //return Redirect::to('/')->with('message', 'Logged in with Facebook');
 		}
+
 		/**
 		 * Show the form for creating a new resource.
 		 *
