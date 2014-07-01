ba8282706077189468c662d88ba6f75da098294b
josece <josece@gmail.com>
2014-06-30 23:01:05 -0500

    limpieza de archivos

--- app/routes.php
+++ app/routes.php
@@ -29,62 +29,15 @@ Route::get('/', function()
 
 Route::get('login', function() { return Redirect::to('user/login'); });
 /* Rutas que requieren sesión iniciada */
-//Route::get('users/login', function() { return Redirect::to('user/login'); });
-//Route::group(array('before' => 'auth'), function() {
-Route::controller('user','usersController');
-//});
-//Route::get('user/logout', function() { Auth::logout(); return Redirect::to('/'); });
 
+Route::controller('user','usersController');
 
 /*
  * View Composer 
  * Crea un objeto $user que es usado siempre que se invoca la plantilla layout/main.blade.php
  */
-View::composer(array('layout.main', 'users.home'), function($view)
-{
+View::composer(array('layout.main', 'users.home'), function($view) {
     $view->with('user',  Auth::user());
 });
 
-//Route::group(array('prefix'=>'user', 'before' => 'auth'), function(){});
-/*Route::group(array('before'=>'auth'), function(){
-	
-	Route::get('user/dashboard','UsersController@showProfile');
-});
-*/
-/*
-Route::get('register', array('uses' =>'RegisterController@index', 'as' =>'register.index'));
-Route::post('register', array('uses' => 'RegisterController@store', 'as'=> 'register.store'));
-
-/* Login nativo*/
-/*Route::get('login', array('uses' => 'SessionController@create','as' => 'session.create'));
-Route::post('login', array('uses' => 'SessionController@store', 'as' => 'session.store'));
-
-/* Login de Facebook 
-Route::get('user/facebok-auth', function() {
-    $facebook = new Facebook(Config::get('facebook'));
-    $params = array('redirect_uri' => url('user/facebok-auth/callback'),'scope' => 'email',);
-    return Redirect::away($facebook->getLoginUrl($params));
-});
-
-/* Logout de sesión */
-/*
-
-Route::get('login', 'UsersController@login');
-
-Route::get('user/register', 'UsersController@register');
-Route::get('user/facebok-auth', 'UsersController@facebookAuth');
-Route::get('user/facebook-auth/callback', 'UsersController@facebookAuthCallback');
-
-Route::post('user/create','UsersController@create');
-Route::post('user/signin','UsersController@signin');
-
-
-
-/* Callback de Login de Facebook 
-	
-    
-	Route::get('user/facebok-auth/callback', function() {
-	   
-	});
-	*/
 	
