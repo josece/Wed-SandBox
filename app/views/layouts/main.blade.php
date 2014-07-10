1b236f48728007f798a41b160fd7ef217f07a259
josece <josece@gmail.com>
2014-07-10 05:56:05 -0500

    correccion de etiquetas

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -126,11 +126,9 @@
 				</div>
 				<div class="large-6 columns  text-right small-7">
 					<ul class="inline-list text--small right list--nomargin">
-						<li>
-							{{Lang::get('global.termsofuse')}}</li> 
+						<li>{{Lang::get('global.termsofuse')}}</li> 
 							<li>{{Lang::get('global.privacynotice')}}</li>
-						</ul>
-					</div>
+					</ul>
 				</div>
 			</div>
 		</footer>
