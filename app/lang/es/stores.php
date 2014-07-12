a49ad8a913413ab1f960f52c73dd02056eef49dc
josece <josece@gmail.com>
2014-07-12 11:57:59 -0500

    traducciones al español

---
+++ app/lang/es/stores.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	"store--title" => "Tienda",
+	"store--new" => "Crear nueva tienda",
+	"store--name" => "Nombre de la tienda",
+	"store--description" => "Descripción",
+	"error--nametaken" => "Por favor elige otro nombre para la tienda",
+	"error--description" => "Dinos un poco de tu tienda, tranquilo, no tiene que ser muy largo",
+	"success--add" => 'La tienda fue creada con éxito',
+	"store--none" => 'No hay ninguna tienda creada todavía. Haz la primera.'
+);
